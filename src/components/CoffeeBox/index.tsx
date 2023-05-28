0b9583566d5d4afb8effe576221a780e54afef7b
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2023-05-28 20:18:13 -0300

    lista de cafés inseridas por um arquivo JSON, falta arrumar as fotos dos cafés

--- src/components/CoffeeBox/index.tsx
+++ src/components/CoffeeBox/index.tsx
@@ -1,28 +1,40 @@
 import { ShoppingCart } from '@phosphor-icons/react'
+import { v4 as uuid } from 'uuid'
 
 import imageOne from '../../assets/coffes/1.svg'
 
-export function CoffeeBox() {
+interface CoffeeBoxProps {
+  id?: number,
+  caracteristics: string[],
+  title: string,
+  description: string,
+  value: number,
+}
+
+export function CoffeeBox({id, caracteristics, title, description, value} :CoffeeBoxProps) {
+
   return (
     <div className='bg-gray-200 w-72 p-4 rounded-r-3xl rounded-b-3xl'>
 
       <img className='-mt-16 mx-auto' src={imageOne} alt="" />
 
       <div className='flex justify-center items-center mt-3 mb-5'>
-        <p className='bg-orange-200 text-orange-800 font-bold text-xs py-2 px-3 rounded-full'>Tradicional</p>
+        {caracteristics.map(caracteristic => {
+          return <p key={uuid()} className='bg-orange-200 text-orange-800 font-bold text-xs py-2 px-3 rounded-full'>{caracteristic}</p>
+        })}
       </div>
 
-      <h2 className='text-center font-bold mb-5 text-2xl'>Expresso Tradicional</h2>
+      <h2 className='text-center font-bold mb-5 text-2xl'>{title}</h2>
 
       <p className='text-center mb-5'>
-        O tradicional café feito com água quente e grãos moídos
+        {description}
       </p>
 
       <div className='flex justify-between items-center px-4 py-2'>
 
         <div className='flex items-end gap-1'>
           <small className='font-bold'>R$</small>
-          <p className='font-bold text-3xl'>9,90</p>
+          <p className='font-bold text-3xl'>{value}</p>
         </div>
 
         <div className='flex items-center gap-3'>
--- src/components/CoffesList/index.tsx
+++ src/components/CoffesList/index.tsx
@@ -1,25 +1,42 @@
+import { useState } from "react";
 import { CoffeeBox } from "../CoffeeBox";
 
+interface ListOfCoffesPattern {
+  id: number,
+  caracteristics: string[],
+  title: string,
+  description: string,
+  value: number,
+}
+
+
+
 export function CoffesList() {
+
+  const [listOfCoffes, setListOfCoffes] = useState<ListOfCoffesPattern[]>([])
+
+  async function captureCoffesList() {
+
+    let coffesList :ListOfCoffesPattern[]= []
+
+    await fetch('/src/db/listOfCoffes.json')
+      .then(response => response.json())
+      .then(data => coffesList = data)
+      .catch(error => console.log(error))
+
+    return setListOfCoffes(coffesList)
+  }
+
+  captureCoffesList()
+
   return (
     <section className="w-4/6 my-0 mx-auto mt-10">
       <h1 className="text-3xl font-bold mb-20">Nossos cafés</h1>
 
       <div className="flex flex-wrap gap-y-28 justify-between">
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
-        <CoffeeBox />
+        {listOfCoffes.map(coffe => {
+          return <CoffeeBox caracteristics={coffe.caracteristics} title={coffe.title} description={coffe.description} value={coffe.value} key={coffe.id} />
+        })}
       </div>
     </section>
   )
